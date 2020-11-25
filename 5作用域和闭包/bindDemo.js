69dda0304c4942de4daf961a0a6739816a953531
sunbowen <857153966@qq.com>
2020-11-25 16:21:14 +0800

    feat:闭包

--- 5作用域和闭包/bindDemo.js
+++ 5作用域和闭包/bindDemo.js
@@ -1,12 +1,40 @@
-function fn1(a, b) {
-    console.log('this', this)
-    console.log(a, b)
-    return 'this is fn1'
+let Person = {
+    name:'Tom',
+    say() {
+        console.log(this)
+        console.log(`我叫${this.name}`)
+    }
 }
-const fn2 = fn1.bind({x:100}, 10, 20)
-const res = fn2();
-console.log(res)
+//代码执行效果
+// Person.say();
 
+let Person1 = {
+    name:'tom1'
+}
+//使用原生call，改变this指向
+// Person.say.call(Person1)
+
+Function.prototype.myCall = function(content) {
+    //content是传入的对象，
+    console.log(this)
+    content.say = this
+    content.say()
+}
+Person.say.myCall(Person1)
+
+//手写call
+// Function.prototype.myCall = function (content) {
+//     if(typeof this !== 'function'){
+//         throw new TypeError('Error')
+//     }
+//     content = content || window;
+//     content.fn = this;
+//     const args = [...arguments].slice(1);
+//     const result = content.fn(...args);
+//     delete content.fn;
+//     return result
+// }
+// fn1.myCall({x:10},1,2,3,4)
 //模拟bind
 Function.prototype.bind = function () {
     //将参数拆解成数组
